42cfea772e0a2fcfadf3b19fbc42008b52105db8
Haegdorens, Gert <gert.haegdorens@yrcw.com>
2020-11-17 04:40:58 -0600

    Adding new logging 1.0.4

--- tests/test_timer.py
+++ tests/test_timer.py
@@ -1,19 +1,22 @@
 import unittest
 from zeppos_core.timer import Timer
-from testfixtures import log_capture
+from testfixtures import LogCapture
+from zeppos_logging.app_logger import AppLogger
 
 class TestTheProjectMethods(unittest.TestCase):
-    @log_capture()
-    def test_start_stop_test_methods(self, capture):
-        timer = Timer()
-        timer.start_timer()
-        timer.pause(time_in_seconds=2)
-        timer.stop_timer()
+    def test_start_stop_test_methods(self):
+        AppLogger.configure_and_get_logger('start_stop_test')
 
-        self.assertEqual(timer.time_elapsed_in_seconds, 2)
-        capture.check(
-                ('zeppos_logging', 'DEBUG', 'Sleep for [2] seconds'),
-        )
+        with LogCapture() as lc:
+            timer = Timer()
+            timer.start_timer()
+            timer.pause(time_in_seconds=2)
+            timer.stop_timer()
+
+            self.assertEqual(timer.time_elapsed_in_seconds, 2)
+            lc.check_present(
+                ('start_stop_test', 'DEBUG', 'Sleep for [2] seconds'),
+            )
 
 
 if __name__ == '__main__':
